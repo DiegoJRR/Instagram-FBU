dd30cf16777322e968a3d8f05f32adb0476f1666
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-06 17:23:27 -0500

    Added basic setup config for Parse backend

--- Instagram-FBU/AppDelegate.m
+++ Instagram-FBU/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        configuration.applicationId = @"myAppId";
+        configuration.server = @"https://instagram-clone-fbu-2020.herokuapp.com//parse";
+    }];
+    
+    [Parse initializeWithConfiguration:config];
+    
     return YES;
 }
 
