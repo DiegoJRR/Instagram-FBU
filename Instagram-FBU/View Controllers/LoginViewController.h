5cfdf5ad5ff6b6091dfe60797bb61cde50adbc22
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-06 18:06:14 -0500

    Added logic to handle the user sign up.
    Checks for missing information, still needs alerts and error messages for the user.

---
+++ Instagram-FBU/View Controllers/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Instagram-FBU
+//
+//  Created by Diego de Jesus Ramirez on 06/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
