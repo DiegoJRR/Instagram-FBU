91a466c0de904725e88e67286a7b8a0c22d76014
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-07 08:27:19 -0500

    Added logout functionality
    Needed to set the id of the login view to LoginViewController, to be able to call the scene delegate and present the login view as the root view when a user is successfully logged out.

---
+++ Instagram-FBU/View Controllers/FeedViewController.h
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.h
+//  Instagram-FBU
+//
+//  Created by Diego de Jesus Ramirez on 07/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
