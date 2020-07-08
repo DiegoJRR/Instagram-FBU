820be10a68b56ff7be2a8ff9c6a5ad18677572b4
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-07 19:13:44 -0500

    Added post view with image and caption fields
    Also removed the unused photo prompt that was placed in FeedViewController. Missing scrollable view in the post composing section

---
+++ Instagram-FBU/View Controllers/ComposeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewController.h
+//  Instagram-FBU
+//
+//  Created by Diego de Jesus Ramirez on 07/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram-FBU/View Controllers/ComposeViewController.m
@@ -0,0 +1,64 @@
+//
+//  ComposeViewController.m
+//  Instagram-FBU
+//
+//  Created by Diego de Jesus Ramirez on 07/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *pictureView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)takePicture:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.pictureView.image = editedImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
