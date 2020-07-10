b44b86aa7ba5f8118cd178f5e4ac1dd21971d645
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-10 09:02:46 -0500

    Created new DetailsViewController and connected the segue

---
+++ Instagram-FBU/View Controllers/DetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.m
+//  Instagram-FBU
+//
+//  Created by Diego de Jesus Ramirez on 09/07/20.
+//  Copyright © 2020 DiegoRamirez. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
